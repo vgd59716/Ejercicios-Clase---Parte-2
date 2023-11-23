04f8eb530fe5772a7135e08705f52fb7edcc07dc
Diego Carbajal Alvarez <vgd59716@educastur.es>
2023-11-23 19:57:01 +0100

    Añado el fichero modificado

--- CalculadoraEuros.java
+++ CalculadoraEuros.java
@@ -5,9 +5,9 @@ public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Por favor, introduce la cantidad en pesetas: ");
 	final double FACTOR_CONV=166.386;
-	int peseta=sc.nextInt();
-	if (peseta<0) System.out.println("La cantidad introducida no es correcta, tiene que ser mayor que cero");
-	else System.out.printf("La cantidad en euros es: %.2f", (peseta/FACTOR_CONV));
+	int pesetas=sc.nextInt();
+	if (pesetas<0) System.out.println("La cantidad introducida no es correcta, tiene que ser mayor que cero");
+	else System.out.printf("La cantidad en euros es: %.2f", (pesetas/FACTOR_CONV));
 	sc.close();
 }
 }
